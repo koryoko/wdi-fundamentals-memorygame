1f986be22bb0ab6ea1203e923764ddfc0fd6847b
Enzo Tolay <spectre@Enzos-MBP.home>
2020-03-16 18:04:41 -0400

    finished?

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+  rank: 'queen',
+  suit: 'hearts',
+  img: 'images/queen-of-hearts.png'
+},
+{
+  rank: 'queen',
+  suit: 'diamonds',
+  img: 'images/queen-of-diamonds.png'
+},
+{
+  rank: 'king',
+  suit: 'hearts',
+  img: 'images/king-of-hearts.png'
+},
+{
+  rank: 'king',
+  suit: 'diamonds',
+  img: 'images/king-of-diamonds.png'
+}
+
 const cardsInPlay = [];
 
 function checkForMatch {
@@ -8,14 +29,24 @@ function checkForMatch {
     console.log("Sorry, try again.");
   }
 
-}
-if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
+  let cardOne = cards[0];
+  cardsInPlay.push(cardOne);
+  console.log("User flipped " + cardOne);
+
+  let cardTwo = cards[1];
+  cardsInPlay.push(cardTwo);
+  console.log("User flipped " + cardTwo);
+
+  let cardThree = cards[2]
+  cardsInPlay.push(cardThree);
+  console.log("User flipped " + cardThree);
+
+  let cardFour = cards[3]
+  cardsInPlay.push(cardFour);
+  console.log("User flipped " + cardFour);
 
 
+function checkForMatch {
 if (cardsInPlay.length === 2 && cardOne === "queen" && cardTwo === "queen") {
   alert("You found a match!")
 
@@ -39,6 +70,15 @@ let cardFour = cards[3]
 cardsInPlay.push(cardFour);
 console.log("User flipped " + cardFour);
 
-function flipCard
-// The instructions were very unclear here. I don't know any of the values and page 6
-// Project:functions didnt give any specifics that I could comprehend.
+
+
+function flipCard();
+
+function createBoard(
+  for (let i = 0; i < cards.length; +=i)
+
+);
+// The instructions were very unclear here especially towards the end. I don't know any of the values and page 6
+// Project:functions didnt give any specifics that I could comprehend. I feel the same way fot the first function up top.
+//Css and HTML were very well detailed while java had 1 minute videos, i feel it should 100% be the other way around.
+//I would really appreciate more videos or spoken word audio files to guide me through this, as I have used youtube more than the cheatsheets or anything else given in the pre-work and still haven't grasped the subject.
