d13dc76d60d68fc27ef816962d2e9bd30c25fe28
Enzo Tolay <spectre@Enzos-MBP.home>
2020-03-16 18:03:01 -0400

    finished?

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,2 +1,44 @@
-let cardOne = "queen";
-let cardTwo = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+function checkForMatch {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+
+}
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+
+if (cardsInPlay.length === 2 && cardOne === "queen" && cardTwo === "queen") {
+  alert("You found a match!")
+
+} else {
+  alert ("Not this time!")
+}
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+let cardThree = cards[2]
+cardsInPlay.push(cardThree);
+console.log("User flipped " + cardThree);
+
+let cardFour = cards[3]
+cardsInPlay.push(cardFour);
+console.log("User flipped " + cardFour);
+
+function flipCard
+// The instructions were very unclear here. I don't know any of the values and page 6
+// Project:functions didnt give any specifics that I could comprehend.
